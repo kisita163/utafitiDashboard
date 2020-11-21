44671685dd1cdd9fc4bc225c51fe0947b3bf2a3c
kubernetes <kisita162@gmail.com>
2020-11-21 13:31:41 -0800

    Dialog form before submitting form

--- src/app/views/material-kit/forms/new-survey/NewSurveyForm.jsx
+++ src/app/views/material-kit/forms/new-survey/NewSurveyForm.jsx
@@ -1,16 +1,29 @@
 import React, { Component } from "react";
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
-import FirebaseAuthService from "../../../../services/firebase/firebaseAuthService"
+import FirebaseAuthService from "../../../../services/firebase/firebaseAuthService";
+import APIClient from "../../../../services/api/apiClient"
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
+
 import {
   Grid
 } from "@material-ui/core";
 import "date-fns";
 import Form from "@rjsf/material-ui";
-//import Form from "@rjsf/fluent-ui";
 import "./Survey.css";
 
 
 class NewSurveyForm extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {dialog: false, formData: {}}
+  }
 
   componentDidMount() {
     // custom rule will have name 'isPasswordMatch'
@@ -27,15 +40,17 @@ class NewSurveyForm extends Component {
     ValidatorForm.removeValidationRule("isPasswordMatch");
   }
 
-  handleSubmit = event => {
+  handleSubmit = () => {
     // Get root reference
     var storageRef = FirebaseAuthService.getStorage();
+    var formData = this.state.formData;
+
     require('crypto').randomBytes(48, function(err, buffer) {
       var token = buffer.toString('hex');
-      console.log(token)
-      event.formData.survey_id = token;
 
-      var file = new File([JSON.stringify(event.formData)], "survey.json");
+      formData.survey_id = token
+
+      var file = new File([JSON.stringify(formData)], "survey.json");
       // Metadata
       var metadata = {
         contentType: "application/json",
@@ -48,18 +63,31 @@ class NewSurveyForm extends Component {
           console.log("Uploaded a blob or file!");
       });
     });
+
+    const apiClient = new APIClient("");
+
+    apiClient.postClientDasboard(formData.sections).then((data) => {
+      console.log("Survey sent...")
+      console.log(this.state.formData)
+    });
   };
 
-  handleChange = event => {
-    event.persist();
-    this.setState({ [event.target.name]: event.target.value });
-  };
 
-  handleDateChange = date => {
-    // console.log(date);
+  handleClickOpen = event => {
+    this.setState({dialog: true,formData: event.formData});
+    
+  }
 
-    this.setState({ date });
-  };
+  handleClose = (fb) => {
+    this.setState({dialog: false});
+    switch(fb) {
+      case 'OK':
+        this.handleSubmit();
+        break;
+      default:
+        return null;
+      }
+  }
 
   render() {
 
@@ -76,12 +104,35 @@ class NewSurveyForm extends Component {
                 schema={schema}
                 uiSchema={uiSchema}
                 formData={formData}
-                onSubmit={this.handleSubmit}
+                onSubmit={this.handleClickOpen}
                 submitOnEnter
                 activityIndicatorEnabled
               />
             </Grid>
           </Grid>
+          <Dialog
+            open={this.state.dialog}
+            onClose={this.handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">
+              {"Are you sure you want to submit this new form?"}
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                Submitting this form means all client applications will be updated with those new data.
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={() => this.handleClose('NOK')} color="primary">
+                Disagree
+              </Button>
+              <Button onClick={() => this.handleClose('OK')} color="primary" autoFocus>
+                Agree
+              </Button>
+            </DialogActions>
+          </Dialog>
       </div>
     );
   }
